1169fb8ef82917c3b681046753cdba7230b98cfc
Henry <henrybulbeck@gmail.com>
2024-08-11 22:12:48 +1000

    Moving to cmake

--- libraries/AP_Progmem/AP_Progmem.h
+++ libraries/AP_Progmem/AP_Progmem.h
@@ -3,11 +3,15 @@
 #define __AP_PROGMEM_H__
 
 #include <AP_HAL_Boards.h>
+/*
 #if defined(__AVR__) 
+*/
 #include "AP_Progmem_AVR.h"
+/*
 #else
 #include "AP_Progmem_Identity.h"
 #endif
+*/
 
 #define PROGMEM_STRING(_v, _s)  static const char _v[] PROGMEM = _s
 
