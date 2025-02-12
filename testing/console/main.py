fdbeda328d8efbd3aebc51f7a36e62f102dbf2d1
Henry <henrybulbeck@gmail.com>
2025-02-12 20:48:59 +1100

    added no async task to ensure non blocking keypress reads

--- testing/console/main.py
+++ testing/console/main.py
@@ -5,16 +5,37 @@ import asyncio
 from queue import SimpleQueue
 
 from protocol import serial_task
-from ui import ui_task
+from ui import ui_task, key_task
+from state import StateManager
+from handler import NavHandler, NoneHandler, LogHandler
 
 async def main(stdscr):
+	curses.curs_set(0)
+	## This sets the screen to refresh non-blocking
+	stdscr.nodelay(True)
+	stdscr.clear()
+
 	cmd_queue = asyncio.Queue()
 	serial_queue = SimpleQueue()
+
+	## TODO Terrible
+	sm = StateManager(
+		NavHandler({
+			'l1': NavHandler({
+				'logs': LogHandler(queues=(serial_queue, cmd_queue)), 
+				'None': NoneHandler(queues=(serial_queue, cmd_queue)),
+				'AlsoNone': NoneHandler(queues=(serial_queue, cmd_queue))
+				}, queues=(serial_queue, cmd_queue)),
+			'l2': NoneHandler(queues=(serial_queue, cmd_queue)),
+			'l3': NoneHandler(queues=(serial_queue, cmd_queue))
+		}, queues=(serial_queue, cmd_queue))
+	)
 
 	## Need to add a third task that handles routing of serial messages to the correct nodes
 	tasks = [
 		asyncio.create_task(serial_task(serial_queue, cmd_queue)),
-		asyncio.create_task(ui_task(stdscr, serial_queue, cmd_queue))
+		asyncio.create_task(ui_task(stdscr, sm, serial_queue, cmd_queue)),
+		asyncio.create_task(key_task(stdscr, sm))
 	]
 
 	await asyncio.gather(*tasks)
--- testing/console/ui.py
+++ testing/console/ui.py
@@ -1,42 +1,29 @@
 
 from helper import eprint
-from state import StateManager
 
-from handler import NavHandler, NoneHandler, LogHandler
 
 import curses
 import asyncio
 
-async def ui_task(stdscr, serial_queue, cmd_queue):
-	curses.curs_set(0)
-	## This sets the screen to refresh non-blocking
-	#stdscr.nodelay(True)
-	stdscr.clear()
-	
+async def key_task(stdscr, sm):
+	while (True):
+		## Blocking
+		key = stdscr.getch()
 
-	## TODO Terrible
-	sm = StateManager(
-		NavHandler({
-			'l1': NavHandler({
-				'logs': LogHandler(queues=(serial_queue, cmd_queue)), 
-				'None': NoneHandler(queues=(serial_queue, cmd_queue)),
-				'AlsoNone': NoneHandler(queues=(serial_queue, cmd_queue))
-				}, queues=(serial_queue, cmd_queue)),
-			'l2': NoneHandler(queues=(serial_queue, cmd_queue)),
-			'l3': NoneHandler(queues=(serial_queue, cmd_queue))
-		}, queues=(serial_queue, cmd_queue))
-	)
+		if key!=-1:
+			sm.respond_keypress(key)
+
+		## I guess this calls the key_task at 10Hz
+		await asyncio.sleep(0.01)
+
+async def ui_task(stdscr, sm, serial_queue, cmd_queue):
 
 	while True:
 		## Trigger drawing of current state
 		sm.current_state.draw(stdscr)
 
-		## Respond to keypress
-		key = stdscr.getch()
-		sm.respond_keypress(key)
-		
-
 		while not serial_queue.empty():
 			message = serial_queue.get()
 
+		## 10Hz
 		await asyncio.sleep(0.1)
