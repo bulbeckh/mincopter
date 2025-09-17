b14eb9e9bc1d8864816cb1cd1a7b5ee16b11dc92
Henry <henrybulbeck@gmail.com>
2025-09-17 11:32:09 +1000

    fixed compass rotation in simulation EXCEPT for the state complementary filter reference vec

--- dev/AP_Compass/sim_compass.cpp
+++ dev/AP_Compass/sim_compass.cpp
@@ -19,6 +19,14 @@ void AP_Compass_Sim::accumulate(void)
 			hal.sim->last_sensor_state.field_z
 			);
 
+	/* Currently, our Gazebo sim is setup so that our compass and imu are rigidly attached to body frame 
+	 * which at the simulation start has x point north, y point left (west) and z up. We negate y and z
+	 * to rotate into NED frame */
+
+	// Rotate into NED frame
+	temp_field.y *= -1;
+	temp_field.z *= -1;
+
 	// Add to the accumulated field
 	acc_field += temp_field;
 	acc_samples += 1;
@@ -50,9 +58,14 @@ bool AP_Compass_Sim::read(void)
 
 	if (acc_samples>0) {
 		// Calculate an average field reading
-		_field.x = acc_field.x / acc_samples;
-		_field.y = acc_field.y / acc_samples;
-		_field.z = acc_field.z / acc_samples;
+		acc_field.x /= acc_samples;
+		acc_field.y /= acc_samples;
+		acc_field.z /= acc_samples;
+
+		// Correct for magnetic field here - _field should be in NED frame now
+		_field.x = acc_field.x;
+		_field.y = acc_field.y;
+		_field.z = acc_field.z;
 
 		// Reset accumulated variables
 		acc_field.zero();
