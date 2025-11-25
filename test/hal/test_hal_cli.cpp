95be92046c5239f0365d64180a6ff5243a00ff41
Henry <henrybulbeck@gmail.com>
2025-11-25 17:55:12 +1100

    working motor test over telemetry

--- test/hal/test_hal_cli.cpp
+++ test/hal/test_hal_cli.cpp
@@ -11,35 +11,101 @@ const AP_HAL::HAL& hal = AP_HAL_BOARD_DRIVER;
 uint8_t run_unit_tests(void)
 {
 	// Init
-
-	uint8_t buff[8] = {0};
 	uint8_t _bc = 0;
 
 	uint32_t loop_time_us = 0;
 
+	uint8_t do_heartbeat = 0;
+
 	/* Here is where we will update the PWM signals and enable them */
 	
-	// Set min throttle for channel 0
-	hal.rcout->write(0, 1000);
-	hal.rcout->enable_ch(0);
+	// Set min throttle for all channels
+	for (uint8_t i=0;i<4;i++) {
+		hal.rcout->write(i, 1000);
+		hal.rcout->enable_ch(i);
+	}
+
+	// Set target_pwm to default of min
+	uint16_t target_pwm[4] = {1000,1000,1000,1000};
 
 	// Run this loop at 100Hz
 	for (int i=0;i<1e6;i++) {
 		uint32_t start = hal.scheduler->micros();
 
 		int16_t nb = hal.console->read();
-		if (nb!=-1) _bc++;
 
-		// Log received characters
-		//hal.console->printf("%.*s\n", _bc==8 ? _bc : _bc+1, buff);
+		if (nb!=-1) {
+			_bc++;
 
-		// Clear buffer
-		for (uint8_t j=0;j<8;j++) buff[j]=0;
+			switch(nb) {
+				// PWM0
+				case 0x60:
+					target_pwm[0] = 1100;
+					break;
+				case 0x61:
+					target_pwm[0] = 1200;
+					break;
+				case 0x62:
+					target_pwm[0] = 1300;
+					break;
 
+				// PWM1
+				case 0x63:
+					target_pwm[1] = 1100;
+					break;
+				case 0x64:
+					target_pwm[1] = 1200;
+					break;
+				case 0x65:
+					target_pwm[1] = 1300;
+					break;
 
+				// PWM2
+				case 0x66:
+					target_pwm[2] = 1100;
+					break;
+				case 0x67:
+					target_pwm[2] = 1200;
+					break;
+				case 0x68:
+					target_pwm[2] = 1300;
+					break;
+
+				// PWM3
+				case 0x69:
+					target_pwm[3] = 1100;
+					break;
+				case 0x6A:
+					target_pwm[3] = 1200;
+					break;
+				case 0x6B:
+					target_pwm[3] = 1300;
+					break;
+
+				// Run setup for 5sec
+				case 0x9F:
+					do_heartbeat = 5;
+
+				default:
+					break;
+			}
+		}
+
+		// If we are currently in a run, we send a signal to PWM0
+		if (do_heartbeat) {
+			hal.rcout->write(0, target_pwm[0]);
+			hal.rcout->write(1, target_pwm[1]);
+			hal.rcout->write(2, target_pwm[2]);
+			hal.rcout->write(3, target_pwm[3]);
+		} else {
+			hal.rcout->write(0, 1000);
+			hal.rcout->write(1, 1000);
+			hal.rcout->write(2, 1000);
+			hal.rcout->write(3, 1000);
+		}
 
 		// Heartbeat every second
-		if (i%100==0) {
+		if (i%100==0 && do_heartbeat) {
 			hal.console->printf("[%luus] heartbeat %d, received %u\r\n", hal.scheduler->micros()-loop_time_us, i, _bc);
 
 			uint16_t pwm_read[4];
@@ -48,11 +114,16 @@ uint8_t run_unit_tests(void)
 			hal.console->printf("\tPWM0 %u, PWM1 %u, PWM2 %u, PWM3 %u\r\n", pwm_read[0], pwm_read[1], pwm_read[2], pwm_read[3]);
 
 			// Toggle the PWM signal every 5 seconds
+			/*
 			if ( (i%1000) / 500) {
 				hal.rcout->write(0, 1000);
 			} else {
 				hal.rcout->write(0, 2000);
 			}
+			*/
+
+			// Decrement by 1 each second
+			do_heartbeat--;
 		}
 
 		// Record loop time
